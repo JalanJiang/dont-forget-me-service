dbd686b15ccfc3e2b9bfb52d3de07b6bd649dc50
JalanJiang <jjy@meitu.com>
2019-01-19 22:44:47 +0800

    [feature]mongoDB 连接方法

---
+++ config/db.js
@@ -0,0 +1,24 @@
+/*
+* MongoDB 连接
+* Time: 2019-01-18 11:38
+* Author: Jalan
+*/
+
+var mongoose = require("mongoose");
+var config = require("../config"); //配置文件
+
+// 数据库连接
+mongoose.connect(config.db.path);
+var db = mongoose.connection;
+
+// 事件监听：错误
+db.on('error', function (err) {
+    console.log('MongoDB connection error:' + err)
+});
+
+// 事件监听：成功
+db.once('open', function () {
+   console.log('MongoDB successfully open!');
+});
+
+exports.mongoose = mongoose;
