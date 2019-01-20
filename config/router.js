23131483ff17e580c2641cc8e054bd60854ef78f
JalanJiang <jjy@meitu.com>
2019-01-20 23:32:40 +0800

    [feature]Add middleware

--- config/router.js
+++ config/router.js
@@ -6,24 +6,27 @@
 module.exports = function (server) {
 
     var account = require('../controller/accountController');
+    var auth = require('../controller/authController');
     var user = require('../controller/userController');
     var book = require('../controller/bookController');
     var note = require('../controller/noteController');
 
     // 鉴权与账号验证
-    server.use(account.checkToken);
-
+    server.use(auth.checkAuth); //鉴权
+    server.use(account.checkToken); //用户 Token 验证
 
     // 账号体系
+    server.post('/v1/account/registry', account.registry); //用户注册
+    server.post('/v1/account/login', account.login); //用户登录
 
     // 用户
 
     // 记本
-    server.post('/v1/book', book.createBook);
-    server.del('/v1/book', book.deleteBook);
-    server.patch('/v1/book', book.updateBook);
-    server.get('/v1/book/:id', book.getBook);
-    server.get('/v1/books', book.getBookList);
+    server.post('/v1/book', book.createBook); //创建记本
+    server.del('/v1/book', book.deleteBook); //删除记本
+    server.patch('/v1/book', book.updateBook); //更新记本
+    server.get('/v1/book/:id', book.getBook); //获取记本详情
+    server.get('/v1/books', book.getBookList); //获取记本列表
 
     // 动态
 };
