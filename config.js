2e953c2beaaadee3dc8491b9f13f01edeff14fcc
JalanJiang <jjy@meitu.com>
2019-01-22 00:47:46 +0800

    [feature]Add salt config

--- config.js
+++ config.js
@@ -6,5 +6,6 @@ module.exports = {
         path: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/dont_forget",
         port: process.env.MONGODB_PORT || "27017"
     },
-    secret: process.env.SECRET || "test_secret"
+    secret: process.env.SECRET || "test_secret",
+    salt: process.env.SALT || "1h029kh2lj11jmjxrg13k1c12b"
 };
